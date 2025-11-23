07828491d1f0e3896490598fdce6df192db4f74d
Jack Bjerregaard <jack@Jacks-MBP.local>
2025-11-23 22:18:49 +0100

    c

---
+++ 3110. Score of a String/C.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <string.h>
+int scoreOfString(char *s) {
+  int sum = 0;
+  int n = strlen(s);
+    for (int i = 0; i < n - 1; i++) {
+      sum += abs(s[i] - s[i + 1]); 
+    }
+  return sum;
+}
